e8b6ab5dc9d2a51d735a2c875c951e2599ddfdf3
varsha-pr-27 <varshaprvviet@gmail.com>
2024-08-13 15:00:36 +0530

    Line added

---
+++ src/ex_08082024/lab0008.py
@@ -0,0 +1,5 @@
+age = 5
+print(type(age))  # <class 'int'>
+
+name = "Varsha"
+print(type(name))  # <class 'str'>
---
+++ src/ex_08082024/lab0009.py
@@ -0,0 +1,7 @@
+# How to take the user input ?
+# Take a username and say Hi, Your name
+
+name = input('Enter your name')
+print('Hi', name, sep="|", end="_")
+print("Chondamma")
+print(type(name))
