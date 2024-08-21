8b5944e82225936fb17b7d9a782fecc1b9c4744f
varsha-pr-27 <varshaprvviet@gmail.com>
2024-08-21 19:42:43 +0530

    New programs added

---
+++ src/ex_08082024/Task 2.py
@@ -0,0 +1,14 @@
+# Create a program , take 2 inputs from the user num1, num2 and give them
+# max
+# pow num1 to num2
+# sub, mul, sum, div.
+# Format your out with f{""}
+
+num1 = int(input("Enter the value of num1"))
+num2 = int(input("Enter the value of num2"))
+
+print(max(f'{num1, num2}'))
+print("Sum of two numbers =", f'{num1 + num2}')
+print("Sub of two numbers =", f'{num1 - num2}')
+print("Mul of two numbers =", f'{num1 * num2}')
+print("Div of two numbers =", f'{num1 / num2}')
