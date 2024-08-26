5f76b25fc3be214df327a6dbda4049e6be74eef6
varsha-pr-27 <varshaprvviet@gmail.com>
2024-08-26 14:59:33 +0530

    New programs added

--- src/ex_08082024/lab00024.py
+++ src/ex_08082024/lab00024.py
@@ -1,7 +1,7 @@
 # Program to print even numbers using  pass keyword
 
-for i in range(1, 10, 1):
+for i in range(1, 10, 1):  # 1, 2, 3, 4, 5, 6, 7, 8, 9
     if i % 2 == 0:
-        print(i)
+        print(i)  # 2, 4, 6 , 8
     else:
         pass
--- src/ex_08082024/lab00025.py
+++ src/ex_08082024/lab00025.py
@@ -1,7 +1,7 @@
-# Continue statement
+# Continue statement along with program to print odd numbers
 
-for number in range(10):
+for number in range(10):  # 1, 2, 3, 4, 5, 6, 7, 8, 9
     if number % 2 == 0:
-        continue
+        continue  # 1, 3, 5, 7, 9
     else:
         print(number)
